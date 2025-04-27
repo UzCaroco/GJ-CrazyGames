d5351567baba3fdeb8cc72e319e54a69060a52c2
Luiz <164922548+UzCaroco@users.noreply.github.com>
2025-04-27 18:59:49 -0300

    testes

--- Assets/Game Logic/Scripts/Player/State Machine/Player State.cs
+++ Assets/Game Logic/Scripts/Player/State Machine/Player State.cs
@@ -4,11 +4,13 @@ public abstract class PlayerState
 {
     protected PlayerStateMachine stateMachine;
     protected PlayerController player;
-
+    protected PlayerInput playerInput;
     public PlayerState (PlayerStateMachine stateMachine, PlayerController player)
     {
         this.stateMachine = stateMachine;
         this.player = player;
+
+        playerInput = new PlayerInput();
     }
 
     public virtual void Enter() { }
--- Assets/Game Logic/Scripts/Player/States/Walk State.cs
+++ Assets/Game Logic/Scripts/Player/States/Walk State.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class WalkState : PlayerState
 {
     public WalkState(PlayerStateMachine stateMachine, PlayerController player) : base(stateMachine, player) { }
-
+    
     public override void Enter()
     {
         Debug.Log("Entrou no Walk State");
+        
     }
 
     public override void Update()
     {
-        /*float x = Input.GetAxisRaw("Horizontal");
-        float z = Input.GetAxisRaw("Vertical");
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
 
-        if (x == 0 && z == 0)
+        /*Vector3 move = new Vector3(x, y, 0);
+        player.transform.Translate(player.transform.position + move.normalized * player.speed * Time.deltaTime);
+
+        if (x == 0 && y == 0)
         {
             stateMachine.ChangeState(player.idleState);
         }
